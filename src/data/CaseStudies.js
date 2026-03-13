0828bc0dfb2792a2fcdd1b56169e9158ef823693
olar <kaffoolar@gmail.com>
2026-03-13 11:16:18 +0100

    case study data

---
+++ src/data/CaseStudies.js
@@ -0,0 +1,108 @@
+export const caseStudies = [
+  {
+    slug: "olar-by-olar",
+    title: "Olar by Olar",
+    coverImg: "/olar/olar-cover.png",
+    painPoint: "A new luxury leather fashion brand that needs brand positioning, clarity and a compelling yet luxurious African identity",
+    risk: "Wrong or poor brand positioning inconsistent brand principles and elements wrong   messaging tone",
+    system: "After getting on a discovery call, we built a system around minimalism and a strong combination of colors to show luxury. Clean and bold typeface with pattern to maintain African originality and a subtle blend of alternating background for flexibility and versatility.",
+    brandLogo: {
+      logo1: "/olar/olar-logo1.png",
+      logo2: "/olar/olar-logo2.png",
+      logo3: "/olar/olar-logo3.png"},
+    logoPresentation: {
+      pres1: "/olar/olar-pres1.png",
+      pres2: "/olar/olar-pres2.png",
+      pres3: "/olar/olar-pres3.png",
+      pres4: "/olar/olar-pres4.png",
+      pres5: "/olar/olar-pres5.png",
+      pres6: "/olar/olar-pres6.png"},
+    flyers: {
+      flyer1: "/olar/olar-flyer1.png",
+      flyer2: "/olar/olar-flyer2.png",
+      flyer3: "/olar/olar-flyer3.png"},
+    website: ""
+  },
+
+
+  {
+    slug: "omma-afromart",
+    title: "Omma's Afromart",
+    coverImg: "/omma/omma-cover.png",
+    painPoint: "A new luxury leather fashion brand that needs brand positioning, clarity and a compelling yet luxurious African identity",
+    risk: "Wrong or poor brand positioning inconsistent brand principles and elements wrong   messaging tone",
+    system: "After getting on a discovery call, we built a system around minimalism and a strong combination of colors to show luxury. Clean and bold typeface with pattern to maintain African originality and a subtle blend of alternating background for flexibility and versatility.",
+    brandLogo: {
+      logo1: "/omma/omma-logo1.png",
+      logo2: "/omma/omma-logo2.png",
+      logo3: "/omma/omma-logo3.png"},
+    logoPresentation: {
+      pres1: "/omma/omma-pres1.png",
+      pres2: "/omma/omma-pres2.png",
+      pres3: "/omma/omma-pres3.png",
+      pres4: "/omma/omma-pres4.png",
+      pres5: "/omma/omma-pres5.png",
+      pres6: "/omma/omma-pres6.png"},
+    flyers: {
+      flyer1: "/omma/omma-flyer1.png",
+      flyer2: "/omma/omma-flyer2.png",
+      flyer3: "/omma/omma-flyer3.png"},
+    website: {
+      web1: "/omma/omma-web1.png",
+      web2: "/omma/omma-web2.png",}
+  },
+
+
+  {
+    slug: "babs-fitness",
+    title: "Babs Fitness",
+    coverImg: "/babs/babs-cover.png",
+    painPoint: "A new luxury leather fashion brand that needs brand positioning, clarity and a compelling yet luxurious African identity",
+    risk: "Wrong or poor brand positioning inconsistent brand principles and elements wrong   messaging tone",
+    system: "After getting on a discovery call, we built a system around minimalism and a strong combination of colors to show luxury. Clean and bold typeface with pattern to maintain African originality and a subtle blend of alternating background for flexibility and versatility.",
+    brandLogo: {
+      logo1: "/babs/babs-logo1.png",
+      logo2: "/babs/babs-logo2.png",
+      logo3: "/babs/babs-logo3.png"},
+    logoPresentation: {
+      pres1: "/babs/babs-pres1.png",
+      pres2: "/babs/babs-pres2.png",
+      pres3: "/babs/babs-pres3.png",
+      pres4: "/babs/babs-pres4.png",
+      pres5: "/babs/babs-pres5.png",
+      pres6: "/babs/babs-pres6.png"},
+    flyers: {
+      flyer1: "/babs/babs-flyer1.png",
+      flyer2: "/babs/babs-flyer2.png",
+      flyer3: "/babs/babs-flyer3.png"},
+    website: ""
+  },
+
+
+  {
+    slug: "raven-ceown-care",
+    title: "Raven's Crown Care",
+    coverImg: "/raven/raven-cover.png",
+    painPoint: "A new luxury leather fashion brand that needs brand positioning, clarity and a compelling yet luxurious African identity",
+    risk: "Wrong or poor brand positioning inconsistent brand principles and elements wrong   messaging tone",
+    system: "After getting on a discovery call, we built a system around minimalism and a strong combination of colors to show luxury. Clean and bold typeface with pattern to maintain African originality and a subtle blend of alternating background for flexibility and versatility.",
+    brandLogo: {
+      logo1: "/raven/raven-logo1.png",
+      logo2: "/raven/raven-logo2.png",
+      logo3: "/raven/raven-logo3.png"},
+    logoPresentation: {
+      pres1: "/raven/raven-pres1.png",
+      pres2: "/raven/raven-pres2.png",
+      pres3: "/raven/raven-pres3.png",
+      pres4: "/raven/raven-pres4.png",
+      pres5: "/raven/raven-pres5.png",
+      pres6: "/raven/raven-pres6.png"},
+    flyers: {
+      flyer1: "/raven/raven-flyer1.png",
+      flyer2: "/raven/raven-flyer2.png",
+      flyer3: "/raven/raven-flyer3.png"},
+    website: {
+      web1: "/raven/raven-web1.png",
+      web2: "/raven/raven-web2.png",}
+  }
+]
