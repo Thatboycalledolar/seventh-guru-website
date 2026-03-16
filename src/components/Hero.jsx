2c00ccec7290385130579ed7a4ecce8d6cdaa5f7
olar <kaffoolar@gmail.com>
2026-03-16 15:10:38 +0100

    hero query done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@
  export default function Hero() {
   return(
     <div className="flex flex-col items-center justify-center w-full h-screen px-8 pt-0 pb-34 bg-linear-to-b from-white to-stone-200">
-      <div className="flex flex-col items-center gap-4 mb-12">
-        <h1 className="text-5xl leading-1.2 text-center text-(--text-color) px-14 font-black tracking-tighter">From ideas. We create. <span className="text-(--primary-color)">You Scale!</span></h1>
-        <p className="text-center font-normal text-(--text-color) text-sm font-body">We Build Digital Infrastructure for Brands ready to grow.</p>
+      <div className="flex flex-col items-center gap-4 mb-12 max-w-300">
+        <h1 className="text-5xl md:text-7xl leading-1.2 text-center text-(--text-color) px-14 font-black tracking-tighter max-w-200">From ideas. We create. <span className="text-(--primary-color)">You Scale!</span></h1>
+        <p className="text-center font-normal text-(--text-color) text-sm md:text-lg font-body">We Build Digital Infrastructure for Brands ready to grow.</p>
       </div>
       <div>
         <MainBtn text="Apply to work with Us" link="/Apply" />
--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -3,9 +3,9 @@ import Contact from "../components/Contact";
 
 export default function Apply() {
   return(
-    <div className="flex flex-col items-center text-(--text-color) mt-8 w-full px-8">
+    <div className="max-w-300 flex flex-col items-center  text-(--text-color) mt-8 w-full px-8">
       <div className="mb-12 flex flex-col">
-        <h1 className="font-bold text-4xl md:text-5xl md:mb-4 text-center ">Apply to work with Us</h1>
+        <h1 className="font-bold text-4xl md:text-6xl md:mb-4 text-center ">Apply to work with Us</h1>
         <p className="text-center">Tell us about your project and we'll see if we're a good fit.</p>
       </div>
       
